d5b3b424796631e0c953ff854223fcc9f33a9406
Sebastien Besson <seb.besson@gmail.com>
2012-08-22 11:36:04 +0100

    Exclude server/ folder from build

--- conf.py
+++ conf.py
@@ -64,7 +64,7 @@ release = '4.4.2'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'trac' , 'sysadmins/source']
+exclude_patterns = ['_build', 'trac' , 'sysadmins/source' ,'server']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
