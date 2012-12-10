3a42fc5f69b351c83a7a9c20a5827c7cf7465e8e
Sebastien Besson <seb.besson@gmail.com>
2012-12-10 11:42:58 +0000

    Remove trac/ folder and exclude formats/ folder at compilation

--- conf.py
+++ conf.py
@@ -76,7 +76,7 @@ else:
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'trac' ]
+exclude_patterns = ['_build', 'formats' ]
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
