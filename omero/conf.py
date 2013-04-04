fb45e76ab832eaf7c05b320e1641336af5f203db
Sebastien Besson <seb.besson@gmail.com>
2013-04-04 23:16:34 +0100

    Remove formats from excluded patterns in OMERO documentation

--- omero/conf.py
+++ omero/conf.py
@@ -83,7 +83,7 @@ else:
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'formats']
+exclude_patterns = ['_build']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
