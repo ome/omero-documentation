1438f24136f07764e2cfb25f80de3651ec2865e6
Helen Flynn <omehelen@gmail.com>
2014-02-20 13:18:43 +0000

    Using right redirects for 5 downloads

--- common/conf.py
+++ common/conf.py
@@ -163,8 +163,8 @@ extlinks = {
     'bf_doc' : (oo_site_root + '/support/bio-formats5/%s', ''),
     'partner_plone' : (oo_site_root + '/products/partner/%s', ''),
     # Downloads
-    'downloads' : (downloads_root + '/latest-develop/omero/%s', ''),
-    'bf_downloads' : (downloads_root + '/latest-develop/bio-formats/%s', ''),
+    'downloads' : (downloads_root + '/latest/omero5/%s', ''),
+    'bf_downloads' : (downloads_root + '/latest/bio-formats5/%s', ''),
     # Help links
     'help' : (help_root + '/%s', ''),
     # Miscellaneous links
