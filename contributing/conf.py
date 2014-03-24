9c4dc2cc98ad66b983bc2b3cd201941f7b6d607c
Sebastien Besson <seb.besson@gmail.com>
2014-03-24 11:23:52 +0000

    Move commit extlink to Contributing documentation

--- contributing/conf.py
+++ contributing/conf.py
@@ -30,8 +30,10 @@ contributing_extlinks = {
     # Github links
     'source' : (omero_github_root + 'blob/'+ branch + '/%s', ''),
     'sourcedir' : (omero_github_root + 'blob/'+ branch + '/%s', ''),
+    'commit' : (omero_github_root + 'commit/%s', ''),
     'bf_source' : (bf_github_root + 'blob/'+ branch + '/%s', ''),
     'bf_sourcedir' : (bf_github_root + 'tree/'+ branch + '/%s', ''),
+    'bf_commit' : (bf_github_root + 'commit/%s', ''),
     }
 extlinks.update(contributing_extlinks)
 
