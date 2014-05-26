c1d2e4f495acca936cd4c082917ee9e4db337725
Simon Li <spli@dundee.ac.uk>
2014-05-26 18:10:37 +0100

    Fix postgres startup, use full path to omero

--- omero/sysadmins/unix/walkthrough/setup_nginx_ubuntu1404.sh
+++ omero/sysadmins/unix/walkthrough/setup_nginx_ubuntu1404.sh
@@ -2,7 +2,7 @@
 
 # See setup_omero.sh for the nginx config file creation
 
-cp OMERO.server/nginx.conf.tmp /etc/nginx/sites-available/omero-web
+cp ~omero/OMERO.server/nginx.conf.tmp /etc/nginx/sites-available/omero-web
 rm /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/omero-web /etc/nginx/sites-enabled/
 
