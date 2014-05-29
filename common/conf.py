7be66152f8fdb05668f52447f8a4acb21db3ef29
Helen Flynn <omehelen@gmail.com>
2014-05-29 11:04:43 +0100

    Edit on GitHub extension for sphinx

--- common/conf.py
+++ common/conf.py
@@ -14,6 +14,7 @@
 
 import datetime
 import sys, os
+sys.path.insert(0, os.path.abspath('_ext'))
 
 def split_release(release):
     import re
@@ -50,6 +51,11 @@ def get_previous_version(majornumber, minornumber=0):
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.)
 extensions = ['sphinx.ext.extlinks']
+extensions = ['edit_on_github']
+
+# Configuration for the edit_on_github extension
+edit_on_github_project = 'openmicroscopy/ome-documentation'
+edit_on_github_branch = 'dev_5_0'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
