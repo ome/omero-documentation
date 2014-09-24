f29b307b0739f722e5296460edb6290cb6c2bf67
Simon Li <spli@dundee.ac.uk>
2014-09-24 15:20:13 +0100

    Updated Linux walkthroughs for OMERO 5.0.5
    Corresponds to https://github.com/manics/omero-install/commit/0a21fde81d644f4cf8054018fc65796529b23056
    
    Note nginx setup has been changed to make it easier to test Apache too (though theses scripts haven't been included in this PR)

--- omero/sysadmins/unix/walkthrough/setup_nginx_centos6.sh
+++ omero/sysadmins/unix/walkthrough/setup_nginx_centos6.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+cat << EOF > /etc/yum.repos.d/nginx.repo
+[nginx]
+name=nginx repo
+baseurl=http://nginx.org/packages/centos/\$releasever/\$basearch/
+gpgcheck=0
+enabled=1
+EOF
+
+yum -y install nginx
+
 # See setup_omero.sh for the nginx config file creation
 
 mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.disabled
