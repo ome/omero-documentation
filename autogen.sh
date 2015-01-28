79b1a50f8082fe80e762a37f654187415a93aaba
Sebastien Besson <seb.besson@gmail.com>
2015-01-28 10:34:03 +0000

    Add echo commands

--- autogen.sh
+++ autogen.sh
@@ -5,15 +5,25 @@ set -e
 set -x
 OMERO_DIR=${OMERO_DIR:-$pwd}
 
+echo "Copying history"
 cp $OMERO_DIR/OMERO.server/history.txt omero/users/
+
+echo "Generating configuration properties page"
 $OMERO_DIR/OMERO.server/bin/omero config parse --rst | sed "s|$OMERO_DIR|/home/omero|" > omero/sysadmins/config.txt
 mkdir -p omero/downloads/ldap
-$OMERO_DIR/OMERO.server/bin/omero ldap setdn -h | sed "s|$OMERO_DIR|/home/omero|" > omero/downloads/ldap/setdn.out || echo "Dumped ldap setdn help"
-$OMERO_DIR/OMERO.server/bin/omero import --advanced-help 2> advanced-help.txt || echo "Dumped advanced CLI help"
+
+echo "Generating ldap setdn usage page"
+$OMERO_DIR/OMERO.server/bin/omero ldap setdn -h | sed "s|$OMERO_DIR|/home/omero|" > omero/downloads/ldap/setdn.out
+
+echo "Generating advanced CLI help"
+$OMERO_DIR/OMERO.server/bin/omero import --advanced-help 2> advanced-help.txt || echo "Dumped"
+sed 1,3d advanced-help.txt > omero/downloads/inplace/advanced-help.txt
+$OMERO_DIR/OMERO.clients/importer-cli -h 2> omero/downloads/cli/help.out || echo "Dumped"
+
+echo "Generating Web configuration temapltes"
 $OMERO_DIR/OMERO.server/bin/omero web config nginx | sed "s|$OMERO_DIR|/home/omero|" > omero/sysadmins/unix/nginx-omero.conf
 $OMERO_DIR/OMERO.server/bin/omero web config apache | sed "s|$OMERO_DIR|/home/omero|" > omero/sysadmins/unix/apache-omero.conf
 $OMERO_DIR/OMERO.server/bin/omero web config apache-fcgi | sed "s|$OMERO_DIR|/home/omero|" > omero/sysadmins/unix/apache-fcgi-omero.conf
 
-sed 1,3d advanced-help.txt > omero/downloads/inplace/advanced-help.txt
-$OMERO_DIR/OMERO.clients/importer-cli -h 2> omero/downloads/cli/help.out || echo "Dumped importer-cli help"
-
+echo "Cleanup"
+rm advanced-help.txt
