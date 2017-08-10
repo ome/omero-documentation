fec67e6ab715fac3d73288266df8d9b59b548626
Snoopy Crime Cop <snoopycrimecop@gmail.com>
2017-08-10 08:18:31 +0000

    Update auto-generated documentation
    Repository: openmicroscopy/ome-documentation
    Already up-to-date.
    
    Generated by OMERO-DEV-latest-docs-autogen#940 (https://ci.openmicroscopy.org/job/OMERO-DEV-latest-docs-autogen/940/)

--- omero/sysadmins/unix/walkthrough/walkthrough_ubuntu1404.sh
+++ omero/sysadmins/unix/walkthrough/walkthrough_ubuntu1404.sh
@@ -21,7 +21,7 @@ apt-get update
 apt-get -y install \
 	unzip \
 	wget \
-	python-{pip,pillow,numpy,scipy,tables,virtualenv,yaml,jinja2}
+	python-{pip,pillow,numpy,tables,virtualenv,yaml,jinja2}
 
 pip install --upgrade pip
 # install Ice
