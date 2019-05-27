4d01cdfc7ddb5b383252af112cbfc059139ed7ef
Jean-Marie Burel <j.burel@dundee.ac.uk>
2019-05-27 14:37:37 +0100

    ignore testng url

--- omero/conf.py
+++ omero/conf.py
@@ -182,7 +182,8 @@ linkcheck_ignore += [r'http://localhost:\d+/?', 'http://localhost/',
     r'https?://www\.openmicroscopy\.org/site/team/.*',
     r'.*[.]?example\.com/.*',
     r'https://spreadsheets.google.com/.*',
-    'https://msdn.microsoft.com/en-us/library/aa362244\(v=vs.85\).aspx'
+    'https://msdn.microsoft.com/en-us/library/aa362244\(v=vs.85\).aspx',
+    r'https://testng.org/*'
 ]
 
 exclude_patterns = ['sysadmins/unix/walkthrough/requirements*',
