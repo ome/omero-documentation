43e75e234742635c39accc97999a8cc3d920717c
jmoore <josh@glencoesoftware.com>
2019-12-18 11:03:12 +0100

    Add slf4j and strandls

--- omero/conf.py
+++ omero/conf.py
@@ -203,6 +203,8 @@ linkcheck_ignore += [
     'https://www.cloudflare.com/',
     'https://www.zenoss.com/',
     r'https://logback\.qos\.ch/.*',
+    r'https://www\.slf4j\.org/.*',
+    r'https://www\.strandls\.com/.*',
 ]
 
 exclude_patterns = ['sysadmins/unix/walkthrough/requirements*',
