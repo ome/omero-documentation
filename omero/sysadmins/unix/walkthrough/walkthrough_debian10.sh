a936c378b0047f375d9576ebba1f97ae21dc6250
jburel <jburel@users.noreply.github.com>
2022-04-26 09:19:22 +0000

    [create-pull-request] automated change

--- omero/sysadmins/unix/walkthrough/walkthrough_debian10.sh
+++ omero/sysadmins/unix/walkthrough/walkthrough_debian10.sh
@@ -81,7 +81,7 @@ $VENV_SERVER/bin/pip install omero-server[default]
 #start-step04-pre: As root, download the OMERO.server
 #start-release-ice36
 cd /opt/omero/server
-SERVER=https://github.com/ome/openmicroscopy/releases/download/v5.6.3/OMERO.server-5.6.3-ice36-b228.zip
+SERVER=https://downloads.openmicroscopy.org/omero/5.6/server-ice36.zip
 wget -q $SERVER -O OMERO.server-ice36.zip
 unzip -q OMERO.server*
 #end-release-ice36
