d7ffef65ce79bc299fa5397b6984e01fa383c9ed
Jean-Marie Burel <j.burel@dundee.ac.uk>
2022-07-02 08:59:16 +0100

    get version from jar

--- omero/dependencies.sh
+++ omero/dependencies.sh
@@ -2,13 +2,36 @@
 # This script is used by a Continuous Integration job to update
 # the versions of the "ome" dependencies
 # We have 3 groups:
-#  - dependencies on artifactories e.g. omero-blitz
+#  - dependencies on artifactories e.g. omero-dsl-plugin
 #  - dependencies on GitHub e.g. omero-insight
 #  - dependencies on pypi e.g. omero-py
+#  - openmicroscopy: 
+#    - Find latest release from GitHub
+#    - Download the latest zip
+#    - check version of the dependencies
 
 PREFIX="omero-"
+
+# General java packages
+# Java packages
+dirs=("org/openmicroscopy/omero-dsl-plugin" "org/openmicroscopy/omero-blitz-plugin")
+for dir in "${dirs[@]}"
+    do
+        : 
+        values=(${dir//// })
+        value=${values[${#values[@]}-1]}
+        v=${value#"$PREFIX"}
+        v=${v//"-"/"_"}
+        echo $v
+        # get the latest version of the package
+        repopath="https://artifacts.openmicroscopy.org/artifactory/ome.releases/${dir}"
+        version=`curl -s ${repopath}/maven-metadata.xml | grep latest | sed "s/.*<latest>\([^<]*\)<\/latest>.*/\1/"`
+        echo $version
+        sed -i -e "s/version_${v} = .*/version_${v} = \"${version}\"/" conf_autogen.py
+    done
+
 # GitHub packages
-new_version=false
+new_version=""
 github_packages=("ome/openmicroscopy" "ome/omero-insight" "ome/omero-matlab")
 for p in "${github_packages[@]}"
 do
@@ -26,7 +49,7 @@ do
         while IFS= read -r line; do
            values=(${line/=/ })
            if [[ ${values[0]} = "version_openmicroscopy" && \"$version\" != ${values[1]} ]]; then
-               new_version=true
+               new_version=$version
            fi
         done < conf_autogen.py
     fi
@@ -37,33 +60,35 @@ done
 echo $new_version
 
 # Java packages
-dirs=("org/openmicroscopy/omero-blitz" "org/openmicroscopy/omero-server" "org/openmicroscopy/omero-gateway"
-      "org/openmicroscopy/omero-romio" "org/openmicroscopy/omero-renderer" "org/openmicroscopy/omero-common"
-      "org/openmicroscopy/omero-model" "org/openmicroscopy/omero-dsl-plugin" "org/openmicroscopy/omero-blitz-plugin"
-      "ome/formats-gpl")
 
-if [ $new_version = "true" ]; then
-
+if [ ! -z $new_version ]; then
+    dirs=("OMERO.server/lib/server/omero-blitz.jar" "OMERO.server/lib/server/omero-server.jar" "OMERO.server/lib/server/omero-gateway.jar"
+      "OMERO.server/lib/server/omero-romio.jar" "OMERO.server/lib/server/omero-renderer.jar" "OMERO.server/lib/server/omero-common.jar"
+      "OMERO.server/lib/server/omero-model.jar" "OMERO.server/lib/server/formats-gpl.jar")
+    # to be modified
+    SERVER=https://downloads.openmicroscopy.org/omero/5.6/server-ice36.zip
+    wget -q $SERVER -O OMERO.server-ice36.zip
+    unzip -q OMERO.server*
+    ln -s OMERO.server-*/ OMERO.server
     for dir in "${dirs[@]}"
     do
-        : 
+        :
         values=(${dir//// })
         value=${values[${#values[@]}-1]}
         v=${value#"$PREFIX"}
-        v=${v//"-"/"_"}
+        version=`unzip -p $dir META-INF/MANIFEST.MF | grep "Implementation-Version:" | sed 's/^.*[^0-9]\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/'`
         echo $v
-        # get the latest version of the package
-        repopath="https://artifacts.openmicroscopy.org/artifactory/ome.releases/${dir}"
-        version=`curl -s ${repopath}/maven-metadata.xml | grep latest | sed "s/.*<latest>\([^<]*\)<\/latest>.*/\1/"`
         echo $version
         sed -i -e "s/version_${v} = .*/version_${v} = \"${version}\"/" conf_autogen.py
     done
+    # clean up 
+    rm -rf OMERO.server*
 fi
 
 
 # Python packages
-packages=("omero-py" "omero-web" "omero-dropbox")
-for package in "${packages[@]}"
+dirs=("omero-py" "omero-web" "omero-dropbox")
+for package in "${dirs[@]}"
 do
     :
     v=${package#"$PREFIX"}
